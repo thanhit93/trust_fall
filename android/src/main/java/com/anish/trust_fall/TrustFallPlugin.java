8a24ca53806124e223b5e3360e3cc280bccfea78
Thanh Truong <truongthanhcn12@gmail.com>
2021-11-22 12:40:01 +0700

    Migrating the plugin to the V2 embedding

--- android/src/main/java/com/anish/trust_fall/TrustFallPlugin.java
+++ android/src/main/java/com/anish/trust_fall/TrustFallPlugin.java
@@ -1,69 +1,45 @@
 package com.anish.trust_fall;
 
 import android.content.Context;
-import android.location.Location;
-import android.os.Looper;
 
-import com.anish.trust_fall.Emulator.EmulatorCheck;
-import com.anish.trust_fall.ExternalStorage.ExternalStorageCheck;
-import com.anish.trust_fall.MockLocation.MockLocationCheck;
-import com.anish.trust_fall.Rooted.RootedCheck;
+import androidx.annotation.NonNull;
 
-import io.flutter.plugin.common.MethodCall;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** TrustFallPlugin */
-public class TrustFallPlugin implements MethodCallHandler {
+public class TrustFallPlugin implements FlutterPlugin {
   /** Plugin registration. */
+  private MethodChannel channel;
+  private MainMethodCallHandler methodCallHandler;
 
-  private final Context context;
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    Context applicationContext = binding.getApplicationContext();
+    BinaryMessenger messenger = binding.getBinaryMessenger();
+    methodCallHandler = new MainMethodCallHandler(applicationContext, messenger);
 
-  @SuppressWarnings("deprecation")
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "trust_fall");
-    channel.setMethodCallHandler(new TrustFallPlugin(registrar.context()));
-  }
+    channel = new MethodChannel(messenger, "trust_fall");
+    channel.setMethodCallHandler(methodCallHandler);
+    binding.getFlutterEngine().addEngineLifecycleListener(new FlutterEngine.EngineLifecycleListener() {
+      @Override
+      public void onPreEngineRestart() {
+        methodCallHandler.dispose();
+      }
 
-  private TrustFallPlugin(Context context){
-    this.context = context;
+      @Override
+      public void onEngineWillDestroy() {
+      }
+    });
   }
 
   @Override
-  public void onMethodCall(MethodCall call, final Result result) {
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else if (call.method.equals("isJailBroken")) {
-      result.success(RootedCheck.isJailBroken(context));
-    } else if (call.method.equals("canMockLocation")) {
-      MockLocationCheck.LocationResult locationResult = new MockLocationCheck.LocationResult(){
-        @Override
-        public void gotLocation(final Location location){
-          android.os.Handler handler = new android.os.Handler(Looper.getMainLooper());
-          handler.post(new Runnable() {
-            @Override
-            public void run() {
-              //Got the location!
-              if(location != null){
-                result.success(location.isFromMockProvider());
-              }else {
-                result.success(false);
-              }
-            }
-          });
-        }
-      };
-      MockLocationCheck mockLocationCheck = new MockLocationCheck();
-      mockLocationCheck.getLocation(context, locationResult);
-    }else if (call.method.equals("isRealDevice")) {
-      result.success(!EmulatorCheck.isEmulator());
-    }else if (call.method.equals("isOnExternalStorage")) {
-      result.success(ExternalStorageCheck.isOnExternalStorage(context));
-    }
-    else {
-      result.notImplemented();
-    }
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    methodCallHandler.dispose();
+    methodCallHandler = null;
+
+    channel.setMethodCallHandler(null);
   }
 }
